9e3cffc465a95c73d4c574b9423b835477a42f6f
李森茂 <lisenmao@qianbaocard.com>
2018-11-20 10:50:32 +0800

    add spring cloud demo

---
+++ segmentfault-dive-in-spring-cloud/src/main/java/com/maomao/SegmentFaultDiveInSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package com.maomao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegmentFaultDiveInSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegmentFaultDiveInSpringCloudApplication.class, args);
+	}
+}
