57c4e6438e7eccc394687df245a320f2e93d2d7c
茂茂 <maomaolsm@yeah.net>
2018-11-29 09:02:28 +0800

    update

--- segmentfault-learning/spring-cloud/learning-5/eureka-client/src/main/java/com/maomao/SpringCloudEurekaClient.java
+++ segmentfault-learning/spring-cloud/learning-5/eureka-client/src/main/java/com/maomao/SpringCloudEurekaClient.java
@@ -2,7 +2,7 @@ package com.maomao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Add some description about this class.
@@ -11,9 +11,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @since 2018/11/28 18:35
  */
 @SpringBootApplication
-@EnableEurekaServer
-public class App {
+@EnableDiscoveryClient
+public class SpringCloudEurekaClient {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(SpringCloudEurekaClient.class, args);
     }
 }
--- segmentfault-learning/spring-cloud/learning-5/eureka-server/src/main/java/com/maomao/SpringCloudEurekaServer.java
+++ segmentfault-learning/spring-cloud/learning-5/eureka-server/src/main/java/com/maomao/SpringCloudEurekaServer.java
@@ -2,6 +2,7 @@ package com.maomao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Add some description about this class.
@@ -10,8 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2018/11/28 18:35
  */
 @SpringBootApplication
-public class App {
+@EnableEurekaServer
+public class SpringCloudEurekaServer {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(SpringCloudEurekaServer.class, args);
     }
 }
