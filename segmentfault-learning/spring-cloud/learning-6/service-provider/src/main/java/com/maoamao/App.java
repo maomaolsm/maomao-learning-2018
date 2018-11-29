b496b1f5b680a688d7a085768a447ea349e576da
李森茂 <lisenmao@qianbaocard.com>
2018-11-29 18:41:04 +0800

    update

---
+++ segmentfault-learning/spring-cloud/learning-6/service-provider/src/main/java/com/maoamao/App.java
@@ -0,0 +1,17 @@
+package com.maoamao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Add some description about this class.
+ *
+ * @author senmao.li
+ * @since 2018/11/29 15:01
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
