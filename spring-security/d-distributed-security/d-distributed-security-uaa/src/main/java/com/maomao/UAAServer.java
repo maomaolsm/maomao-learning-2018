95d2560cd8328163b4405cb1a874bb80890ac0a2
maomao <meomeolsm@yean.net>
2020-06-23 14:19:42 +0800

    update

--- spring-security/d-distributed-security/d-distributed-security-uaa/src/main/java/com/maomao/UAAServer.java
+++ spring-security/d-distributed-security/d-distributed-security-uaa/src/main/java/com/maomao/UAAServer.java
@@ -15,4 +15,10 @@ public class UAAServer {
 	public static void main(String[] args) {
 		SpringApplication.run(UAAServer.class, args);
 	}
+
+	// 获取code
+	// http://localhost:53020/uaa/oauth/authorize?client_id=c1&response_type=code&scope=all&redirect_uri=http://www.baidu.com
+	// 拿到code，换token
+	// http://localhost:53020/uaa/oauth/token?client_id=c1&client_secret=secret&grant_type=authorization_code&code=5PgfcD&redirect_uri=http://www.baidu.com
+
 }
---
+++ spring-security/d-distributed-security/d-distributed-security-uaa/src/main/java/com/maomao/service/SpringDataUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.maomao.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringDataUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        System.out.println("username=" + s);
+        UserDetails userDetails = User.withUsername("zhang")
+                .password("$2a$10$lY5aS.N8q3883kMNHmM6Q.TVwvqMMcxjRRbISOSJXxPsEaaN7Kuoi")
+                .authorities("p1")
+                .build();
+        return userDetails;
+    }
+}
